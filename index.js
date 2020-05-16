1360c01e9471dffa745ffecc798afd46d5e4b317
Ashley George <ashley@microserve.io>
2020-05-16 19:03:57 +0100

    Use PORT from env.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
   res.send('hello')
 })
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 });
