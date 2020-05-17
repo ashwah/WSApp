4b93900b8d11c3d80ae5080f18a0a1ab857f8f8a
Ashley George <ashley@microserve.io>
2020-05-17 13:35:17 +0100

    Adding postgres and core api routes.

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
+
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;
-const { v4: uuidv4 } = require('uuid');
+const routes = require('./routes/api');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res, next) => {
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-  res.send(uuidv4())
-})
+// Use Bodyparser middleware.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Serve the API routes.
+app.use('/api', routes);
+
+// Start the app.
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
