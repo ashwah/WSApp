8ad39c7cf3463f19412f02a7284e2ff92b08f471
Ashley George <ashley@microserve.io>
2020-05-17 16:58:46 +0100

    Add catchall route to hopefully serve the react app?

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const PORT = process.env.PORT || 5000;
 
 const express = require('express')
 const app = express()
-const routes = require('./routes/api');
-const bodyParser = require('body-parser');
+const routes = require('./routes/api')
+const bodyParser = require('body-parser')
+const path = require('path')
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,8 +17,18 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 // Serve the API routes.
 app.use('/api', routes);
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 // Start the app.
 app.listen(PORT, () => {
