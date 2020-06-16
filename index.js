13386a6b0f4bc56d88f6839e2983c61bc5587831
Ashley George <ashley@microserve.io>
2020-06-16 15:23:57 +0100

    Change public back to build.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
 
@@ -31,7 +31,7 @@ app.use('/api', routes);
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 
