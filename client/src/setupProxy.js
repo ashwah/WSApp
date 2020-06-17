1641f20e7fb67d63a4d5e7cc56b990de05938b38
Ashley George <ashley@microserve.io>
2020-06-17 17:15:16 +0100

    Revmoing proxies.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,14 +1,14 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-console.log('in setupproxy');
-const wsProxy = createProxyMiddleware('/ws', {
-  target:'http://localhost:8080',
-  logLevel: 'debug',
-  secure: true,
-  //changeOrigin: true,
-  ws: true});
-const apiProxy = createProxyMiddleware('/test', {target:'http://localhost:5000'});
-
-module.exports = function(app) {
-  app.use(wsProxy);
-  // /app.use(apiProxy);
-};
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// console.log('in setupproxy');
+// const wsProxy = createProxyMiddleware('/ws', {
+//   target:'http://localhost:8080',
+//   logLevel: 'debug',
+//   secure: true,
+//   //changeOrigin: true,
+//   ws: true});
+// const apiProxy = createProxyMiddleware('/test', {target:'http://localhost:5000'});
+//
+// module.exports = function(app) {
+//   app.use(wsProxy);
+//   // /app.use(apiProxy);
+// };
